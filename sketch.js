d1bddeb33f5d605260b17e7a32e727c657a785a2
LukmanSingla <79403472+LukmanSingla@users.noreply.github.com>
2021-03-22 17:33:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function setup() {
     c1=new Box(1100,250,20,150,"white");
     c2=new Box(1300,250,20,150,"white");
     c3=new Box(1200,320,220,20,"white");
-    ball=Bodies.circle(50,320,20,{restitution:1,density:1.2,friction:1});
+    ball=Bodies.circle(50,320,20,{density:1.2,friction:1,restitution:1});
 	ground=new Ground(700,340,1400,20,"yellow");
   World.add(world,ball);
 	Engine.run(engine);
@@ -35,14 +35,11 @@ function draw() {
   fill("pink");
   ellipseMode(RADIUS);
   ellipse(ball.position.x,ball.position.y,20,20);
-keyPressed();
   ground.display();
   drawSprites();
+  if(keyWentDown("up")){
+    Body.applyForce(ball, ball.position, {x:90,y:-60});
+  }
 }
 
-function keyPressed(){
-  if(keyCode===UP_ARROW){
-    Body.applyForce(ball, ball.position, {x:1,y:-3})
-  } 
-}
 
